d1c32ff142429e639d4ea691313c2240acaac0e0
Alessio_Grieco <Alessiogrieco218@gmail.com>
2024-11-18 20:14:22 +0100

    Aggiunto Stile

---
+++ Es_2/main.js
@@ -0,0 +1,59 @@
+// TRACCIA
+
+// Inserire nel file html due campi input ed un pulsante per generare un articolo:
+// un campo per inserire un titolo
+// un campo per inserire un paragrafo
+// inserire nell’articolo anche la data di pubblicazione tramite questa istruzione:
+// Date - JavaScript | MDN 
+
+
+// let date = new Date();
+// let formatDate = date.toLocaleDateString()
+
+
+// Implementare la seguente funzionalità:
+// al click del pulsante, dovrete creare un articolo popolato dai valori prelevati dai due campi input
+
+
+// Potete prendere spunto da questa anteprima:
+
+let btn = document.querySelector('button');
+const cardContainer = document.getElementById('card-container');
+
+// Aggiungo l'event listener al bottone "Crea Articolo"
+btn.addEventListener("click", () => {
+    // Ottieni il valore del titolo e del paragrafo
+    let title = document.getElementById("title").value;
+    let paragraph = document.getElementById("paragraph").value;
+    let date = new Date();
+    let formatDate = date.toLocaleDateString();
+
+    // Verifico se è stato inserito un testo
+    if (title === '' || paragraph === '') {
+        alert('Per poter continuare, inserire un testo');
+        return;
+    }
+
+    // Crea una nuova card
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    // Crea il titolo della card
+    const cardTitle = document.createElement('h3');
+    cardTitle.textContent = title;
+
+    // Crea il paragrafo della card
+    const cardParagraph = document.createElement('p');
+    cardParagraph.textContent = paragraph;
+
+    // Aggiungo il titolo e il paragrafo alla card
+    card.appendChild(cardTitle);
+    card.appendChild(cardParagraph);
+
+    // Aggiungo la card al contenitore delle card
+    cardContainer.appendChild(card);
+
+    // Pulisci gli input
+    document.getElementById("title").value = '';
+    document.getElementById("paragraph").value = '';
+});
